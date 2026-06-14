7a600dbc1b63869aa001e363675adf4a6ae6fd82
Rahad0Islam <vairahad99@gmail.com>
2026-06-14 23:24:39 +0600

    Add SQL queries for match details, user search, and booking status checks

--- QUERY.sql
+++ QUERY.sql
@@ -96,3 +96,28 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+
+
+-- query 1
+
+SELECT match_id,fixture,base_ticket_price
+FROM matches
+WHERE tournament_category = 'Champions League';
+
+
+-- query 2
+SELECT user_id,full_name,email
+FROM users
+WHERE full_name ILIKE 'tanvir%'
+or full_name ILIKE '%Haque';
+
+-- query 3
+
+SELECT booking_id,user_id,match_id,COALESCE(payment_status,'Action Required')as systematic_status
+FROM bookings
+WHERE payment_status is NULL;
+
+
+
+
